f4d5d2a464fddc49884cfcb35da332532786cfd7
sangheonEN <jteks6@gmail.com>
2020-08-24 15:27:20 +0900

    struct_class

---
+++ assignment operator/assignment operator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            a += 20;
+            // a = a + 20;
+            Console.WriteLine(a);
+
+            a++;
+            // a = a + 1;
+            Console.WriteLine(a);
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ struck_class/struck_class/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struck_class
+{
+    struct AA { }
+    class BB { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AA a;              // heap 영역의 AA라는 메모리영역이 할당됨
+
+            BB b;              // heap 영역에서 null을 참조하다가 new로 생성자를 만들어주면 Heap에 BB라는 Class의 메모리 영역이 할당된다.
+
+            BB c = new BB();   // new로 생성자를 만들어주면 Heap에 BB라는 Class의 메모리가 할당됨. c라는 인스턴스로 접근가능!
+        }
+    }
+}
