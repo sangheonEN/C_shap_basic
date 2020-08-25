1423700304df7ee8b0e2b9be65cb1f8ce5f3b36d
sangheonEN <jteks6@gmail.com>
2020-08-25 14:58:34 +0900

    Equals()메서드를 사용해서 데이터 값이 같은지 비교해야 되고 ==을 사용한다는 얘기는 각 변수가 가르치고 있는 주소값이 같은지 확인하는 거라서 DB할때는 문제가 될 수 있다.

---
+++ Study_Foreach/Study_Foreach/Mycl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_Foreach
+{
+    class Mycl
+    {
+        public int A;
+        public int B;
+        public int C;
+    }
+}
---
+++ Study_Foreach/Study_Foreach/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Mycl cl1 = new Mycl();
+            cl1.A = 10;
+            cl1.B = 20;
+            cl1.C = 30;
+            Mycl cl2 = new Mycl();
+            cl2.A = 100;
+            cl2.B = 200;
+            cl2.C = 300;
+            Mycl cl3 = new Mycl();
+            cl3.A = 1000;
+            cl3.B = 2000;
+            cl3.C = 3000;
+
+            Mycl[] mycls_ary = { cl1, cl2, cl3 };
+
+            foreach (Mycl item in mycls_ary)
+            {
+                Console.WriteLine($"item.A = {item.A}");
+                Console.WriteLine($"item.B = {item.B}");
+                Console.WriteLine($"item.C = {item.C}");
+            }
+
+        }
+    }
+}
