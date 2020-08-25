637cf7f95356e347cd5f13dbfef45eb227d32231
sangheonEN <jteks6@gmail.com>
2020-08-25 14:41:00 +0900

    Contains_요소가 포함되었는지 안되었는지 확인하는 bool 반환형 메서드

---
+++ Contains/Contains/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool test = "abc".Contains("a");                 // abc 문자열에 a가 들어있는지??
+            Console.WriteLine($"test = {test}");
+
+            string b = "abc";
+
+            if(test.Equals(b)) // 데이터 비교
+            {
+                Console.WriteLine("데이터는 같습니다.");
+            }
+
+            string a = "abc";
+
+            if(a == b)        // 주소 값 비교
+            {
+                Console.WriteLine("주소 값이 같습니다!!");
+            }
+
+        }
+    }
+}
