217b612c8234d2af042d33662acb63985187dfba
sangheonEN <jteks6@gmail.com>
2020-08-27 15:10:49 +0900

    Class

---
+++ Class_20200827/Class_20200827/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_20200827
+{
+    class Car
+    {
+        public string carname;
+        public int carnumber;
+        public string carcolor;
+
+        public void printCar()
+        {
+            Console.WriteLine($"{carname}, {carnumber}, {carcolor}");
+        }
+    }
+
+    class A
+    {
+        public int b = 10;
+
+        public static int a = 10;
+        public static void doa()
+        {
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //static으로 선언되었을때 클래스의 변수, 함수 사용법
+            Console.WriteLine(A.a);   // static이니까 생성자 없이 바로 사용할 수 있음
+            A.doa();                  // static이니까 생성자 없이 바로 사용할 수 있음
+
+            A A1 = new A();           // static 변수가 아니면 생성자를 선언해주고 생성자 변수를 가지고 Class의 변수를 사용 가능함.
+            Console.WriteLine(A1.b);
+
+
+            //
+            List<Car> list = new List<Car>();
+            list.Add(new Car() { carname = "기아", carnumber = 1, carcolor = "레드" });
+            list.Add(new Car() { carname = "현대", carnumber = 2, carcolor = "보라" });
+            list.Add(new Car() { carname = "삼성", carnumber = 3, carcolor = "블루" });
+
+            foreach (Car item in list)
+            {
+                item.printCar();
+            }
+
+
+        }
+    }
+}
---
+++ index/index/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace index
+{
+    // index 문법
+    class A
+    {
+        private int a = 10;
+        private int b = 1;
+        private int c = 1;
+
+        public int this[int index]
+        {
+            get { return a * index; }  // 값을 반환
+            set { a = value; b = value; c = value; }
+        }
+
+        private int myprop = 100;
+        public int MyProperty { get { return myprop; } set { myprop = value; } }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            Console.WriteLine(a[10]);
+            a[10] = 20;
+            a[5] = 100;
+            Console.WriteLine(a[10]);
+            Console.WriteLine(a[5]);
+
+            // get : 값을 호출하면 반환하는 개념 set : 값을 바꿔주는 개념
+            Console.WriteLine(a.MyProperty);
+            a.MyProperty = 200;
+            Console.WriteLine(a.MyProperty);
+        }
+    }
+}
