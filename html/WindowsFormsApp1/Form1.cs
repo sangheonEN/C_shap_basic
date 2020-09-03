8203a01b0cb293429f334611fdf3f5095cdfa508
sangheonEN <jteks6@gmail.com>
2020-09-03 15:44:48 +0900

    html
    html++

---
+++ html/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "http://jteks5.dothome.co.kr";
+            WebClient client = new WebClient();
+            string html = client.DownloadString(url);
+            textBox1.Text = html;
+            
+        }
+    }
+}
