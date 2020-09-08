64a8c4646ee24b9f97867938a30d9f534bd67711
sangheonEN <jteks6@gmail.com>
2020-09-08 11:54:56 +0900

    index

--- index/index/Program.cs
+++ index/index/Program.cs
@@ -13,7 +13,14 @@ namespace index
         private int b = 1;
         private int c = 1;
 
-        public int this[int index]
+        public int this[int index, int made]                    // overlode
+        {
+            get {
+                Console.WriteLine("get이 호출");
+                return a * made * index; }
+            set { Console.WriteLine("set이 호출"); ; }
+        }
+        public int this[int index]                              // overlode
         {
             get { return a * index; }  // 값을 반환
             set { a = value; b = value; c = value; }
@@ -27,16 +34,20 @@ namespace index
         static void Main(string[] args)
         {
             A a = new A();
-            Console.WriteLine(a[10]);
-            a[10] = 20;
-            a[5] = 100;
-            Console.WriteLine(a[10]);
-            Console.WriteLine(a[5]);
 
-            // get : 값을 호출하면 반환하는 개념 set : 값을 바꿔주는 개념
-            Console.WriteLine(a.MyProperty);
-            a.MyProperty = 200;
-            Console.WriteLine(a.MyProperty);
+            a[10, 20] = 20;                          //set 호출
+            Console.WriteLine($"{a[10, 20]}");       //get 호출
+
+            //Console.WriteLine(a[10]);
+            //a[10] = 20;
+            //a[5] = 100;
+            //Console.WriteLine(a[10]);
+            //Console.WriteLine(a[5]);
+
+            //// get : 값을 호출하면 반환하는 개념 set : 값을 바꿔주는 개념
+            //Console.WriteLine(a.MyProperty);
+            //a.MyProperty = 200;
+            //Console.WriteLine(a.MyProperty);
         }
     }
 }
